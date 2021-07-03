fb752e5b2d5008bb7b31223460a6977d3053d918
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-07-03 12:38:15 +0200

    Move MainActivity to presentation package

---
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yolo.fun_habit_journal.framework.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yolo.fun_habit_journal.R
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
