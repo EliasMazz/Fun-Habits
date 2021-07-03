e8b1e8401b364094532f71d07fe5c32d53945b87
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-07-03 12:38:01 +0200

    Add BaseApplication

---
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.yolo.fun_habit_journal.framework.presentation
+
+import android.app.Application
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
