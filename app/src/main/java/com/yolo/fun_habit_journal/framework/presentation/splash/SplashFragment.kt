119548bd60154ddc693969be47063227551efd7c
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-07-03 12:37:49 +0200

    Add SplashFragment

---
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashFragment.kt
@@ -0,0 +1,17 @@
+package com.yolo.fun_habit_journal.framework.presentation.splash
+
+import android.os.Bundle
+import android.view.View
+import com.yolo.fun_habit_journal.R
+import com.yolo.fun_habit_journal.framework.presentation.common.BaseFragment
+
+class SplashFragment : BaseFragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun inject() {
+        TODO("Not yet implemented")
+    }
+}
