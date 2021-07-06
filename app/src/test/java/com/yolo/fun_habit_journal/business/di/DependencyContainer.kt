e3650468a71031a7ab89302c1bcc78b85420357f
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-06 22:02:36 +0200

    Remove comments

---
+++ app/src/test/java/com/yolo/fun_habit_journal/business/di/DependencyContainer.kt
@@ -0,0 +1,52 @@
+package com.yolo.fun_habit_journal.business.di
+
+import com.yolo.fun_habit_journal.business.data.HabitDataFactory
+import com.yolo.fun_habit_journal.business.data.cache.FakeHabitCacheDataSource
+import com.yolo.fun_habit_journal.business.data.cache.abstraction.IHabitCacheDataSource
+import com.yolo.fun_habit_journal.business.data.network.FakeHabitNetworkDataSource
+import com.yolo.fun_habit_journal.business.data.network.abstraction.IHabitNetworkDataSource
+import com.yolo.fun_habit_journal.business.domain.model.Habit
+import com.yolo.fun_habit_journal.business.domain.model.HabitFactory
+import com.yolo.fun_habit_journal.business.domain.util.DateUtil
+import com.yolo.fun_habit_journal.framework.util.isUnitTest
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
+
+class DependencyContainer {
+
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss a", Locale.ENGLISH)
+    private val dateUtil = DateUtil(dateFormat)
+
+    lateinit var habitNetworkDataSource: IHabitNetworkDataSource
+    lateinit var habitCacheDataSource: IHabitCacheDataSource
+    lateinit var habitFactory: HabitFactory
+    lateinit var habitDataFactory: HabitDataFactory
+
+    init {
+        isUnitTest = true
+    }
+
+    lateinit var habitsData: HashMap<String, Habit>
+
+    fun build() {
+        this.javaClass.classLoader?.let { classLoader ->
+            habitDataFactory = HabitDataFactory(classLoader)
+
+            habitsData = habitDataFactory.produceHashMapOfHabits(
+                habitDataFactory.produceListOfHabits()
+            )
+        }
+        habitFactory = HabitFactory(dateUtil)
+        habitNetworkDataSource = FakeHabitNetworkDataSource(
+            habitsData = habitsData,
+            deletedHabitsData = HashMap(),
+            dateUtil = dateUtil
+        )
+
+        habitCacheDataSource = FakeHabitCacheDataSource(
+            habitsData = habitsData,
+            dateUtil = dateUtil
+        )
+    }
+}
---
+++ app/src/test/java/com/yolo/fun_habit_journal/business/usecases/habitlist/InsertNewHabitUseCaseTest.kt
@@ -0,0 +1,5 @@
+package com.yolo.fun_habit_journal.business.usecases.habitlist
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class InsertNewHabitUseCaseTest
