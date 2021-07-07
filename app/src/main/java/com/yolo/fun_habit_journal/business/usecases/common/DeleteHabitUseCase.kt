00bcf2dfe3f50040733be93ccdef80e11cf46ee2
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-07 20:01:09 +0200

    Create DeleteHabitUseCase

--- app/src/main/java/com/yolo/fun_habit_journal/business/usecases/common/DeleteHabitUseCase.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/business/usecases/common/DeleteHabitUseCase.kt
@@ -1,5 +1,85 @@
 package com.yolo.fun_habit_journal.business.usecases.common
 
-class DeleteHabitUseCase{
+import com.yolo.fun_habit_journal.business.data.cache.abstraction.IHabitCacheDataSource
+import com.yolo.fun_habit_journal.business.data.cache.util.CacheResponseHandler
+import com.yolo.fun_habit_journal.business.data.cache.util.safeCacheCall
+import com.yolo.fun_habit_journal.business.data.network.abstraction.IHabitNetworkDataSource
+import com.yolo.fun_habit_journal.business.data.network.util.safeApiCall
+import com.yolo.fun_habit_journal.business.domain.model.Habit
+import com.yolo.fun_habit_journal.business.domain.state.DataState
+import com.yolo.fun_habit_journal.business.domain.state.MessageType
+import com.yolo.fun_habit_journal.business.domain.state.Response
+import com.yolo.fun_habit_journal.business.domain.state.StateEvent
+import com.yolo.fun_habit_journal.business.domain.state.UIComponentType
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
+const val DELETE_HABIT_SUCCESS = "Succesfully deleted the habit"
+const val DELETE_HABIT_FAILURE = "Failed to delet the habit"
+
+class DeleteHabitUseCase<ViewState>(
+    private val habitCacheDataSource: IHabitCacheDataSource,
+    private val habitNetworkDataSouce: IHabitNetworkDataSource
+) {
+    fun deleteHabit(
+        habit: Habit,
+        stateEvent: StateEvent
+    ): Flow<DataState<ViewState>?> = flow {
+
+        val cacheResult = safeCacheCall(IO) {
+            habitCacheDataSource.deleteHabit(habit.id)
+        }
+
+        val response = object : CacheResponseHandler<ViewState, Int>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override fun handleSuccess(result: Int): DataState<ViewState> {
+                return handleCacheSuccess(stateEvent, result)
+            }
+        }.getResult()
+
+        emit(response)
+
+        updateNetwork(
+            message = response?.stateMessage?.response?.message,
+            habit = habit
+        )
+    }
+
+    private fun handleCacheSuccess(stateEvent: StateEvent, result: Int): DataState<ViewState> =
+        if (result > 0) {
+            DataState.data(
+                response = Response(
+                    message = DELETE_HABIT_SUCCESS,
+                    uiComponentType = UIComponentType.None,
+                    messageType = MessageType.Success
+                ),
+                data = null,
+                stateEvent = stateEvent
+            )
+        } else {
+            DataState.data(
+                response = Response(
+                    message = DELETE_HABIT_FAILURE,
+                    uiComponentType = UIComponentType.Toast,
+                    messageType = MessageType.Error
+                ),
+                data = null,
+                stateEvent = stateEvent
+            )
+        }
+
+    private suspend fun updateNetwork(message: String?, habit: Habit) {
+        if (message == DELETE_HABIT_SUCCESS) {
+            safeApiCall(IO) {
+                habitNetworkDataSouce.deleteHabit(habit.id)
+            }
+
+            safeApiCall(IO) {
+                habitNetworkDataSouce.insertDeletedHabit(habit)
+            }
+        }
+    }
 }
