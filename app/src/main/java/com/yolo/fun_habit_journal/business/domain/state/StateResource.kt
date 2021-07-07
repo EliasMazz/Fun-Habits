753d2e491e501fc7aadf048548cd02307c414183
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-07 19:51:35 +0200

    Format

--- app/src/main/java/com/yolo/fun_habit_journal/business/domain/state/StateResource.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/business/domain/state/StateResource.kt
@@ -13,9 +13,9 @@ data class Response(
 
 sealed class UIComponentType{
 
-    class Toast: UIComponentType()
+    object Toast : UIComponentType()
 
-    class Dialog: UIComponentType()
+    object Dialog : UIComponentType()
 
     class AreYouSureDialog(
         val callback: AreYouSureCallback
@@ -26,18 +26,18 @@ sealed class UIComponentType{
         val onDismissCallback: TodoCallback? = null
     ): UIComponentType()
 
-    class None: UIComponentType()
+    object None : UIComponentType()
 }
 
 sealed class MessageType{
 
-    class Success: MessageType()
+    object Success : MessageType()
 
-    class Error: MessageType()
+    object Error : MessageType()
 
-    class Info: MessageType()
+    object Info : MessageType()
 
-    class None: MessageType()
+    object None : MessageType()
 }
 
 
