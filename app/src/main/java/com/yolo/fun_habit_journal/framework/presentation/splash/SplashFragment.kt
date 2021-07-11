e8d8f5edd511b618269cf61c4619a32976a25f77
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-12 00:23:36 +0200

    Update splash fragment

--- app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashFragment.kt
@@ -3,11 +3,16 @@ package com.yolo.fun_habit_journal.framework.presentation.splash
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.yolo.fun_habit_journal.R
+import com.yolo.fun_habit_journal.business.domain.state.DialogInputCaptureCallback
+import com.yolo.fun_habit_journal.framework.datasource.network.EMAIL
 import com.yolo.fun_habit_journal.framework.presentation.BaseApplication
 import com.yolo.fun_habit_journal.framework.presentation.common.BaseFragment
+import com.yolo.fun_habit_journal.framework.util.printLogD
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 
@@ -18,23 +23,58 @@ constructor(
     private val viewModelFactory: ViewModelProvider.Factory
 ): BaseFragment(R.layout.fragment_splash) {
 
-    val viewModel: SplashViewModel by viewModels {
+    private val viewModel: SplashViewModel by viewModels {
         viewModelFactory
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        navHabitListFragment()
+        checkFirebaseAuth()
     }
 
-    private fun navHabitListFragment(){
+    private fun checkFirebaseAuth(){
+        if(FirebaseAuth.getInstance().currentUser == null){
+            displayCapturePassword()
+        }
+        else{
+            subscribeObservers()
+        }
+    }
+
+    // add password input
+    private fun displayCapturePassword(){
+        uiController.displayInputCaptureDialog(
+            getString(R.string.text_enter_password),
+            object: DialogInputCaptureCallback {
+                override fun onTextCaptured(text: String) {
+                    FirebaseAuth.getInstance()
+                        .signInWithEmailAndPassword(EMAIL, text)
+                        .addOnCompleteListener {
+                            if(it.isSuccessful){
+                                printLogD("MainActivity",
+                                    "Signing in to Firebase: ${it.result}")
+                                subscribeObservers()
+                            }
+                        }
+                }
+            }
+        )
+    }
+
+    private fun subscribeObservers(){
+        viewModel.hasSyncBeenExecuted().observe(viewLifecycleOwner, Observer { hasSyncBeenExecuted ->
+            if(hasSyncBeenExecuted){
+                navNoteListFragment()
+            }
+        })
+    }
+
+    private fun navNoteListFragment(){
         findNavController().navigate(R.id.action_splashFragment_to_habitListFragment)
     }
 
     override fun inject() {
-        activity?.run {
-            (application as BaseApplication).appComponent
-        }?: throw Exception("AppComponent is null.")
+        getAppComponent().inject(this)
     }
 }
 
--- app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashViewModel.kt
@@ -1,6 +1,24 @@
 package com.yolo.fun_habit_journal.framework.presentation.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class SplashViewModel : ViewModel() {
+@Singleton
+class SplashViewModel
+@Inject
+constructor(
+    private val habitNetworkSyncManager: HabitNetworkSyncManager
+): ViewModel(){
+
+    init {
+        syncCacheWithNetwork()
+    }
+
+    fun hasSyncBeenExecuted() = habitNetworkSyncManager.hasSyncBeenExecuted
+
+    private fun syncCacheWithNetwork() {
+        habitNetworkSyncManager.executeDataSync(viewModelScope)
+    }
 }
