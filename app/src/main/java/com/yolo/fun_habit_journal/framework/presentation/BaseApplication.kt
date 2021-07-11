148e512108174b165d0253de44f87f567239f132
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-11 13:26:21 +0200

    Init app component on Apllication

--- app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/BaseApplication.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/BaseApplication.kt
@@ -1,10 +1,24 @@
 package com.yolo.fun_habit_journal.framework.presentation
 
 import android.app.Application
+import com.yolo.fun_habit_journal.dependencyinjection.AppComponent
+import com.yolo.fun_habit_journal.dependencyinjection.DaggerAppComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
-class BaseApplication : Application() {
+@FlowPreview
+@ExperimentalCoroutinesApi
+open class BaseApplication : Application() {
+
+    lateinit var appComponent: AppComponent
 
     override fun onCreate() {
         super.onCreate()
+        initAppComponent()
+    }
+
+    open fun initAppComponent() {
+        appComponent = DaggerAppComponent.factory()
+            .create(this)
     }
 }
