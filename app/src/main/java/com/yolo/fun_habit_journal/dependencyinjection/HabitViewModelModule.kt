034a4d0dc7cc71d622ae5b2e80aefdf904cd53ab
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-11 16:18:16 +0200

    Create HabitViewModelModule

---
+++ app/src/main/java/com/yolo/fun_habit_journal/dependencyinjection/HabitViewModelModule.kt
@@ -0,0 +1,39 @@
+package com.yolo.fun_habit_journal.dependencyinjection
+
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModelProvider
+import com.yolo.fun_habit_journal.business.domain.model.HabitFactory
+import com.yolo.fun_habit_journal.business.usecases.habitdetail.HabitDetailInteractors
+import com.yolo.fun_habit_journal.business.usecases.habitlist.HabitListInteractors
+import com.yolo.fun_habit_journal.framework.presentation.common.HabitViewModelFactory
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@FlowPreview
+@Module
+object HabitViewModelModule {
+
+    @Singleton
+    @JvmStatic
+    @Provides
+    fun provideHabitViewModelFactory(
+        habitListInteractors: HabitListInteractors,
+        habitDetailInteractors: HabitDetailInteractors,
+        habitFactory: HabitFactory,
+        editor: SharedPreferences.Editor,
+        sharedPreferences: SharedPreferences
+    ): ViewModelProvider.Factory {
+        return HabitViewModelFactory(
+            habitListInteractors = habitListInteractors,
+            habitDetailInteractors = habitDetailInteractors,
+            habitFactory = habitFactory,
+            editor = editor,
+            sharedPreferences = sharedPreferences
+        )
+    }
+
+}
