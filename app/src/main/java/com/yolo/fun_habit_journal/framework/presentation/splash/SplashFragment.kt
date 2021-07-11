6b47edae19bdbd53e871d94ba35e2209b9de4cf1
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-11 21:06:43 +0200

    Navigate to HabitListFragment

--- app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.yolo.fun_habit_journal.R
+import com.yolo.fun_habit_journal.framework.presentation.BaseApplication
 import com.yolo.fun_habit_journal.framework.presentation.common.BaseFragment
-import com.yolo.fun_habit_journal.framework.presentation.common.HabitViewModelFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
-class SplashFragment constructor(
+@FlowPreview
+@ExperimentalCoroutinesApi
+class SplashFragment
+constructor(
     private val viewModelFactory: ViewModelProvider.Factory
-) : BaseFragment(R.layout.fragment_splash) {
+): BaseFragment(R.layout.fragment_splash) {
 
     val viewModel: SplashViewModel by viewModels {
         viewModelFactory
@@ -18,9 +24,17 @@ class SplashFragment constructor(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navHabitListFragment()
+    }
+
+    private fun navHabitListFragment(){
+        findNavController().navigate(R.id.action_splashFragment_to_habitListFragment)
     }
 
     override fun inject() {
-        TODO("Not yet implemented")
+        activity?.run {
+            (application as BaseApplication).appComponent
+        }?: throw Exception("AppComponent is null.")
     }
 }
+
