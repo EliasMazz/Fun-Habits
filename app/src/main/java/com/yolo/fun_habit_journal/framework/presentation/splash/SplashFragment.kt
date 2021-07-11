7487839496e0a1266b49e42bf6a7d88731186f24
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-11 16:11:07 +0200

    Add SplashViewModel to SplashFragment

--- app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/splash/SplashFragment.kt
@@ -2,10 +2,19 @@ package com.yolo.fun_habit_journal.framework.presentation.splash
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.yolo.fun_habit_journal.R
 import com.yolo.fun_habit_journal.framework.presentation.common.BaseFragment
+import com.yolo.fun_habit_journal.framework.presentation.common.HabitViewModelFactory
 
-class SplashFragment : BaseFragment(R.layout.fragment_splash) {
+class SplashFragment constructor(
+    private val viewModelFactory: ViewModelProvider.Factory
+) : BaseFragment(R.layout.fragment_splash) {
+
+    val viewModel: SplashViewModel by viewModels {
+        viewModelFactory
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
