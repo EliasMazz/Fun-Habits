29267fa9a35267542e55fa3dbae3dae21bd966a2
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-11 16:17:53 +0200

    Inject HabitListViewModel to HabitListFragment

--- app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/habitlist/HabitListFragment.kt
+++ app/src/main/java/com/yolo/fun_habit_journal/framework/presentation/habitlist/HabitListFragment.kt
@@ -2,12 +2,28 @@ package com.yolo.fun_habit_journal.framework.presentation.habitlist
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.yolo.fun_habit_journal.R
+import com.yolo.fun_habit_journal.business.domain.util.DateUtil
 import com.yolo.fun_habit_journal.framework.presentation.common.BaseFragment
+import com.yolo.fun_habit_journal.framework.presentation.common.HabitViewModelFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
 const val HABIT_LIST_STATE_BUNDLE_KEY = "com.yolo.fun_habit_journal.framework.presentation.habitlist.state"
 
-class HabitListFragment : BaseFragment(R.layout.fragment_habit_list) {
+@ExperimentalCoroutinesApi
+@FlowPreview
+class HabitListFragment constructor(
+    private val viewModelFactory: ViewModelProvider.Factory,
+    private val dateUtil: DateUtil
+) : BaseFragment(R.layout.fragment_habit_list) {
+
+    val viewModel: HabitListViewModel by viewModels {
+        viewModelFactory
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
