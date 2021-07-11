38995c5ad945a6099fadd4b6da942d1146d4a77e
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-07-11 13:22:14 +0200

    Create HabitDetailInteractors

---
+++ app/src/main/java/com/yolo/fun_habit_journal/business/usecases/habitdetail/HabitDetailInteractors.kt
@@ -0,0 +1,10 @@
+package com.yolo.fun_habit_journal.business.usecases.habitdetail
+
+import com.yolo.fun_habit_journal.business.usecases.common.usecase.DeleteHabitUseCase
+import com.yolo.fun_habit_journal.business.usecases.habitdetail.usecase.UpdateHabitUseCase
+import com.yolo.fun_habit_journal.framework.presentation.habitdetail.state.HabitDetailViewState
+
+class HabitDetailInteractors(
+    val deleteHabit: DeleteHabitUseCase<HabitDetailViewState>,
+    val updateHabitUseCase: UpdateHabitUseCase
+)
