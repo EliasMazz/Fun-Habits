8521723a7b3e7b0b3176ad56d1622b7e11c65588
Elias Mazzocco <elias.mazzocco@gmail.com>
2021-08-10 22:51:23 +0200

    Create ExpressoIdlingResource Rule

---
+++ app/src/androidTest/java/com/yolo/fun_habits/framework/util/EspressoIdlingResourceRule.kt
@@ -0,0 +1,28 @@
+package com.yolo.fun_habits.framework.util
+
+import androidx.test.espresso.IdlingRegistry
+import com.yolo.fun_habits.framework.util.printLogD
+import com.yolo.fun_habits.util.EspressoIdlingResource
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class EspressoIdlingResourceRule : TestWatcher(){
+
+    private val CLASS_NAME = "EspressoIdlingResourceRule"
+
+    private val idlingResource = EspressoIdlingResource.countingIdlingResource
+
+    override fun finished(description: Description?) {
+        printLogD(CLASS_NAME, "FINISHED")
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        super.finished(description)
+    }
+
+    override fun starting(description: Description?) {
+        printLogD(CLASS_NAME, "STARTING")
+        IdlingRegistry.getInstance().register(idlingResource)
+        super.starting(description)
+    }
+
+
+}
